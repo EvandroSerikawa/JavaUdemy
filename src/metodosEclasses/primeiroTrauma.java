7c53ddc2513e24b12410986627edd5113da6ad29
Evandro Serikawa <serikawabass@hotmail.com>
2022-08-22 00:50:17 -0300

    Exercicios aula 109

--- src/metodosEclasses/primeiroTrauma.java
+++ src/metodosEclasses/primeiroTrauma.java
@@ -1,4 +1,11 @@
 package metodosEclasses;
 
 public class primeiroTrauma {
+
+    int a =3; // não pode mexer aqui
+
+    public static void main(String[] args) {
+        // Desafio - corrigir o codigo sem alterar a linha da variavel a
+        //System.out.println(a);
+    }
 }
